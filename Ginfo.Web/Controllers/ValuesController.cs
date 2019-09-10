6d65466ea2605f831bcf13af383c473714c510c7
Filip Hrabec <filip.hrabec@svt.cz>
2019-09-10 15:35:48 +0200

    Test for docker build

--- Ginfo.Web/Controllers/ValuesController.cs
+++ Ginfo.Web/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Ginfo.Web.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "tstststs", "value2" };
         }
 
         // GET api/values/5
